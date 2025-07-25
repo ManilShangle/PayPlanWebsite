39ea3f81698267ddf17f3f2559d302f6a6efc882
ManilShangle <manilshangle@gmail.com>
2025-07-25 17:22:13 -0500

    changed name

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/PayPlan-Landing-Page">
+      <BrowserRouter basename="/PayPlan">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
