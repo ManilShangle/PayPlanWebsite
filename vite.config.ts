14b2d687888b8eee59a65795976b61a4c1e38621
ManilShangle <manilshangle@gmail.com>
2025-08-08 12:57:51 -0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/PayPlanWebsite/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    componentTagger(),
-  ],
+export default defineConfig({
+  base: '/PayPlanWebsite/',
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+})
